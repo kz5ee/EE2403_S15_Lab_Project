3614e0772a0c86f6e536a33fbd7603df953cc886
KZ5EE <kz5ee@yahoo.com>
2015-02-13 21:00:16 -0600

    Set oscillator to use FRC + PLL @140MHz

---
+++ EE2403_Wx_Stn.X/main.c
@@ -0,0 +1,50 @@
+/* 
+ * File:   main.c
+ * Author: Mike
+ *
+ * Created on February 13, 2015, 7:48 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <p24exxxx.h>
+
+/*
+ * Words of Power (Configuration Words)
+ * Refer to lines 50,605 - 50,961 in the PIC42EP512GU810.h header file.
+ */
+
+_FGS(GWRP_OFF & GSS_OFF & GSSK_OFF)
+_FOSCSEL(FNOSC_FRC & IESO_OFF)
+_FOSC(FCKSM_CSECMD & OSCIOFNC_ON & POSCMD_NONE)
+_FWDT( WDTPOST_PS8192 & WDTPRE_PR32 & WINDIS_OFF & FWDTEN_OFF)
+_FPOR( FPWRT_PWR128 & BOREN_ON)
+_FICD( ICS_PGD1 & JTAGEN_OFF)
+_FAS(AWRP_OFF & APL_OFF & APLK_OFF)
+
+
+int main(int argc, char** argv) {
+    void Ludacris_Speed_GO(void); //Function to set FOSC=140MHz
+    Ludacris_Speed_GO();
+
+
+
+
+
+    return (EXIT_SUCCESS);
+}
+
+inline void Ludacris_Speed_GO(void)
+{
+    // Configure PLL prescaler, PLL postscaler, PLL divisor
+PLLFBD=63; // M=65
+CLKDIVbits.PLLPOST=0; // N2=2
+CLKDIVbits.PLLPRE=1; // N1=3
+// Initiate Clock Switch to FRC oscillator with PLL (NOSC=0b001)
+__builtin_write_OSCCONH(0x01);
+__builtin_write_OSCCONL(OSCCON | 0x01);
+// Wait for Clock switch to occur
+while (OSCCONbits.COSC!= 0b001);
+// Wait for PLL to lock
+while (OSCCONbits.LOCK!= 1);
+}
