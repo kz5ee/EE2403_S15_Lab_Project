99f2e67a11baf7db3d66620231482f27f267458b
KZ5EE <kz5ee@yahoo.com>
2015-02-27 17:32:15 -0600

    Project for Temperature and Pressure MCU.

---
+++ Wx_Stn_TP.X/main.c
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.c
+ * Author: Mike
+ *
+ * Created on February 27, 2015, 5:26 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <xc.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
