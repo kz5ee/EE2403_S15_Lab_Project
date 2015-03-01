77f5ec109899a2981a7ec03a0c3fd45617f20e2a
KZ5EE <kz5ee@yahoo.com>
2015-03-01 12:17:53 -0600

    Start of frontend for weather station.

---
+++ Weather_Station_Dashboard/Weather Station Dashboard/Form1.vb
@@ -0,0 +1,13 @@
+﻿Public Class Form1
+
+    Private Sub frmMain_KeyDown(ByVal sender As Object, ByVal e As System.Windows.Forms.KeyEventArgs) Handles MyBase.KeyDown
+        If e.KeyCode = Keys.F12 Then
+            MessageBox.Show("F12 has been pressed", "Keypress Handled")
+        End If
+    End Sub
+
+    Private Sub tmrKeyStroke_Tick(sender As Object, e As EventArgs) Handles tmrKeyStroke.Tick
+
+    End Sub
+
+End Class
