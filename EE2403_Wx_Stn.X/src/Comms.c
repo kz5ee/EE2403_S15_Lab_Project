165fdd8ec2be56a1b7e960832dbc7a0c7f195fa4
KZ5EE <kz5ee@yahoo.com>
2015-04-15 05:54:41 -0500

    Added SPI setup code to test the wireless module.

--- EE2403_Wx_Stn.X/src/Comms.c
+++ EE2403_Wx_Stn.X/src/Comms.c
@@ -184,7 +184,33 @@ void UART4_Config(void){
 //</editor-fold>
 
 //<editor-fold defaultstate="collapsed" desc="SPI Configuration">
+   void SPI1_Config(void)
+    {
+        IFS0bits.SPI1IF = 0; // Clear the Interrupt flag
+        IEC0bits.SPI1IE = 0; // Disable the interrupt
 
+        // SPI setup code for pic24EP512GU810
+        SPI1STATbits.SPIEN = 0;         // Disable SPI 1 for setup
+        SPI1STATbits.SPISIDL=0;         // Operate during IDLE mode
+        SPI1STATbits.SPIEN = 0;         // Enable SPI 1 after setup
+
+        SPI1CON1bits.DISSCK = 0;	// SPIx clock on SCKx pin is enabled
+        SPI1CON1bits.DISSDO = 0;	// SDOx pin is controlled by the module
+        SPI1CON1bits.MODE16 = 0;	// Communication is byte-wide (8 bits)
+        SPI1CON1bits.SMP = 0;           // Input data is sampled at the middle of data output time
+        SPI1CON1bits.CKE =	1;	// Serial output data changes on transition from active clock state to Idle clock state
+        SPI1CON1bits.SSEN = 0;          // SSx pin is not used by the module; pin is controlled by port function
+        SPI1CON1bits.CKP =	0;	// Idle state for clock is a low level; active state is a high level
+        SPI1CON1bits.MSTEN = 1;         // Master mode
+        SPI1CON1bits.SPRE = 0b110;	// Secondary prescale 2:1
+        SPI1CON1bits.PPRE= 0b11;	// Primary prescale 1:1
+
+        SPI1CON2bits.FRMEN = 0;         // Framed SPIx support is disabled
+        SPI1CON2bits.SPIFSD = 0;	// Frame sync pulse output (master)
+        SPI1CON2bits.FRMPOL = 0;	// Frame sync pulse is active-low
+        SPI1CON2bits.FRMDLY = 0;	// Frame sync pulse precedes the first bit clock
+        SPI1CON2bits.SPIBEN = 0;	// Enhanced buffer is disabled (Legacy mode)
+    }
 ///</editor-fold>
 
 //<editor-fold defaultstate="collapsed" desc="I2C Configuration">
@@ -202,7 +228,9 @@ int anprintf(const char *format,...)
 
     do
     {
-        SPI1BUF = wireless_buff[i];
+        while(!SPI1STATbits.SPITBF)
+        SPI1BUF = wireless_buff[i];  //Send a character to the wireless module
+        wireless_buff[i] = SPI1BUF;  //Read character sent from wireless module
         i++;
     } while(wireless_buff[i] != '\0');
 
